780e4fb49272e4757eb827f854bbcde0f8c81155
Stephen Anthony <stephen@blockworks.co>
2023-03-24 18:47:06 -0500

    subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,12 @@ const Home: NextPage = () => {
 
       <main className="min-h-screen bg-gray-200 py-10">
         <div className="container mx-auto">
-          <h1 className="text-4xl font-bold mb-10 text-center text-teal-500">
+          <h1 className="text-4xl font-bold mb-2 text-center text-teal-500">
             SoulGuru
           </h1>
+          <h2 className="text-lg font-medium mb-10 text-center text-gray-500">
+            Your ChatGPT Spiritual Guide
+          </h2>
           <Chatbot />
         </div>
       </main>
